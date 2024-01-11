cea46a9b84b4f8599df0d3daadbda3e64af345c2
cdz-bash <54128190+cdz-bash@users.noreply.github.com>
2024-01-12 03:56:14 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -27,6 +27,8 @@ git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app
 svn export https://github.com/haiibo/packages/trunk/luci-app-vssr package/luci-app-vssr
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2/luci-app-passwall2 package/luci-app-passwall2
+git clone --depth=1 https://github.com/vernesong/OpenClash/luci-app-openclash package/luci-app-openclash
 svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
 svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
